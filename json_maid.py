343f4623154cfb6bad607e2ab5d72c242dbab290
rgb_beard <62804472+rgbbeard@users.noreply.github.com>
2021-10-28 08:39:38 +0200

    Update json_maid.py

--- json_maid.py
+++ json_maid.py
@@ -2,79 +2,79 @@ import json
 from os import path
 
 class JSONMaid():
-	__connection = None
-	__database: str = ""
+__data = None
+__storage: str = ""
 
-	def __init__(self, database: str = "database.json"):
-		if path.isfile(database) and ".json" in database:
-			self.__database = database
+def __init__(self, filename: str = "storage.json"):
+	if path.isfile(filename) and ".json" in filename:
+		self.__storage = filename
 
-			with open(database, "r") as data:
-				data = data.read()
-				self.__connection = self.__from_json(data)
+		with open(filename, "r") as data:
+			data = data.read()
+			self.__data = self.__from_json(data)
 
-				if self.__connection["data"] != dict:
-					self.__connection["data"] = dict()
+			if self.__data["data"] != dict:
+				self.__data["data"] = dict()
 
-	def __del__(self):
-		self.__connection = None
+def __del__(self):
+	self.__data = None
 
-	def __from_json(self, data: str):
-		return json.loads(data)
+def __from_json(self, data: str):
+	return json.loads(data)
 
-	def __to_json(self, data: dict):
-		return json.dumps(data)
+def __to_json(self, data: dict):
+	return json.dumps(data)
 
-	def __save(self):
-		data = self.__to_json(self.__connection)
-		database = open(self.__database, "w")
-		database.write(data)
-		database.close()
+def __save(self):
+	data = self.__to_json(self.__data)
+	storage = open(self.__storage, "w")
+	storage.write(data)
+	storage.close()
 
-	def get_records(self):
-		return self.__connection["data"]
+def get_records(self):
+	return self.__data["data"]
 
-	def records_count(self):
-		return len(self.get_records())
+def records_count(self):
+	return len(self.get_records())
 
-	def delete_record(self, id: int):
-		data = self.get_records()
+def delete_record(self, id: int):
+	data = self.get_records()
 
-		try:
-			del data[str(id)]
-		except:
-			return False
-		finally:
-			self.__save()
+	try:
+		del data[str(id)]
+	except:
+		return False
+	finally:
+		self.__save()
+		return True
+
+def update_record(self, old_record: dict, new_record: dict):
+	data = self.get_records()
+
+	for x in range(1, self.records_count() + 1):
+		row = data[str(x)]
+
+		if row == old_record:
+			row = new_record
 			return True
 
-	def update_record(self, old_record: dict, new_record: dict):
-		data = self.get_records()
+	return False
 
-		for x in range(1, self.records_count() + 1):
-			row = data[str(x)]
-
-			if row == old_record:
-				row = new_record
-				return True
-				
-		return False
-
-	def put_record(self, record: dict):
-		can_be_added = True
+def put_record(self, record: dict):
+	can_be_added = True
     
-    for x in range(1, self.records_count() + 1):
-      row = self.get_records()[str(x)]
+	for x in range(1, self.records_count() + 1):
+		row = self.get_records()[str(x)]
 
-      if row == record:
-        can_be_added = False
-        break
+		if row == record:
+			can_be_added = False
+			break
 
-    if can_be_added:
-      uid = self.records_count() + 1
-      uid = str(uid)
+	if can_be_added:
+		uid = self.records_count() + 1
+		uid = str(uid)
 
-      self.__connection["data"][uid] = record
-      self.__save()
+		self.__data["data"][uid] = record
+		self.__save()
 
-		return can_be_added
+	return can_be_added
