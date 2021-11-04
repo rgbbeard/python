c4fcbcc903832f85622b082c6b291438a5990bb9
rgb_beard <62804472+rgbbeard@users.noreply.github.com>
2021-11-04 11:27:25 +0100

    Create thread_maid.py

---
+++ thread_maid.py
@@ -0,0 +1,31 @@
+import threading
+
+
+class ThreadMaid:
+	__thread = None
+	__thread_target = None
+	__thread_arguments = tuple()
+
+	def __init__(self):
+		pass  # Just instantiate the class
+
+	def setup(self, target, arguments: tuple = ()):
+		self.__set_target(target)
+		self.__set_arguments(arguments)
+		self.__thread = Thread(target=self.__thread_target, args=self.__thread_arguments)
+    return self
+
+	def __set_target(self, t):
+		self.__thread_target = t
+
+	def __set_arguments(self, a: tuple):
+		if len(arguments) > 0:
+			self.__thread_arguments = a
+
+	def halt(self):
+		if self.__thread != None:
+			self.__thread.stop()
+
+	def run(self):
+		if self._thread != None:
+			self.__thread.start()
