6a57897fb2d80902ad20d225b421268018a89861
rgb_beard <62804472+rgbbeard@users.noreply.github.com>
2021-11-05 16:24:46 +0100

    Update app_utils.py

--- app_utils.py
+++ app_utils.py
@@ -7,9 +7,20 @@ import os
 from datetime import datetime
 import socket
 from time import ctime
+import pyunpack
+import re
+
 
 def str_split(string: str):
     return [char for char in string]
+
+
+def is_dir(target: str):
+    return os.path.isdir(target)
+
+
+def is_file(target: str):
+    return os.path.isfile(target)
 
 
 def fsize(target: str):  # File/folder size
@@ -29,18 +40,27 @@ def fcdate(target: str):  # Date creation
         return ctime(fmdate(target))
     return None
 
-def is_dir(target: str):
-    return os.path.isdir(target)
 
-
-def is_file(target: str):
-    return os.path.isfile(target)
-
-
-def abspath(target: str):
-    if is_file(target) or is_dir(target):
-        return os.path.abspath(target)
+def fname(target: str):
+    if is_file(target):
+        name, ext = os.path.splitext(target)
+        return name
     return ""
+
+
+def fext(target: str):
+    if is_file(target):
+        name, ext = os.path.splitext(target)
+        return ext[1::]  # Remove the leading dot
+    return ""
+
+
+def is_xml(target: str):
+    if is_file(target) and "xml" in fext(target):  # Verify file's extension
+        with open(target, "r") as tmp_xml:
+            if re.match(r"(<\?xml version=\".+\" encoding=\".+\"\?>)", tmp_xml[0].strip()):  # Verify header
+                return True
+    return False
 
 
 def frecent(target: str):
@@ -61,6 +81,12 @@ def frecent(target: str):
     days = datetime.fromtimestamp(seconds).strftime("%d")
 
     return int(days) <= 1
+
+
+def abspath(target: str):
+    if is_file(target) or is_dir(target):
+        return os.path.abspath(target)
+    return ""
 
 
 def get_path(from_filename: str, path_format: str = "lunix"):
@@ -84,3 +110,11 @@ def ip():
   h = socket.gethostname()
   ip = socket.gethostbyname(h)
   return ip
+
+
+def un7zip(target: str, destination: str):  # Requires pyunpack and patool installed
+    if is_file(target) and fext(target) == "7z":
+        try:
+            pyunpack.Archive(target).extractall(destination)
+        except Exception as e:
+            print(e)
