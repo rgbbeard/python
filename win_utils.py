9a9bca837d6fc21f6a948efc7a0ff9122c8d8468
rgb_beard <62804472+rgbbeard@users.noreply.github.com>
2021-11-05 16:16:43 +0100

    Update win_utils.py

--- win_utils.py
+++ win_utils.py
@@ -8,10 +8,6 @@ from os import system as cmd
 import pyunpack
 
 
-ROOT = "s:/dati_cliente/municipia"
-SFTP = "d:/utentisftp/municipia"
-
-
 def fmove_local(target: str, destination: str):
 	if (is_file(target) or is_dir(target)) and is_dir(destination):
 		cmd(f'move ./"{target}" "{destination}"')
@@ -20,11 +16,3 @@ def fmove_local(target: str, destination: str):
 def frename_local(target: str, new_name: str):
 	if is_file(target) or is_dir(target):
 		cmd(f"rename {target} {new_name}")
-
-
-def unzip(target: str, destination: str):  # Requires pyunpack and patool installed
-	if is_file(target) and fext(target) == "7z":
-		try:
-			pyunpack.Archive(target).extractall(destination)
-		except Exception as e:
-			print(e)
