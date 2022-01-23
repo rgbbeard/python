4b159e3c61d3dc2c0d64d3ab4bce0301d98a16fb
rgb_beard <62804472+rgbbeard@users.noreply.github.com>
2022-01-23 12:07:38 +0100

    Update thread_maid.py

--- thread_maid.py
+++ thread_maid.py
@@ -1,18 +1,22 @@
 import threading
+from ctypes import (pythonapi, py_object)
 
 
 class ThreadMaid:
 	__thread = None
+	thread_id = 0
 	__thread_target = None
 	__thread_arguments = tuple()
 
 	def __init__(self):
 		pass  # Just instantiate the class
 
-	def setup(self, target, arguments: tuple = ()):
+	def setup(self, target: object, arguments: tuple = ()):
 		self.__set_target(target)
 		self.__set_arguments(arguments)
 		self.__thread = threading.Thread(target=self.__thread_target, args=self.__thread_arguments)
+		self.thread_id = self.__get_id()
+
 		return self
 
 	def __set_target(self, t):
@@ -22,9 +26,24 @@ class ThreadMaid:
 		if len(a) > 0:
 			self.__thread_arguments = a
 
+	def __get_id(self):
+		if hasattr(self.__thread, '_thread_id'):
+			return self.__thread._thread_id
+
+		for id, thread in threading._active.items():
+			if thread == self:
+				return id
+
 	def halt(self):
 		if self.__thread != None:
-			self.__thread.stop()
+			try:
+				thread_stopped = pythonapi.PyThreadState_SetAsyncExc(self.thread_id, py_object(SystemExit))
+
+				if thread_stopped > 1:
+					pythonapi.PyThreadState_SetAsyncExc(self.thread_id, 0)
+
+			except Exception as e:
+				raise Exception(f"Unable to quit Thread ID: {self.thread_id}")
 
 	def run(self):
 		if self._thread != None:
