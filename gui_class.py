bd97d223ef7fc763ac691d3933a1abc199ff3297
rgb_beard <62804472+rgbbeard@users.noreply.github.com>
2022-05-26 10:43:10 +0200

    Update gui_class.py

--- gui_class.py
+++ gui_class.py
@@ -28,7 +28,10 @@ ARIAL_SMALL = (FONT_FAMILY, 15)
 
 
 class Window:
-    window = None
+    __window = None
+    __window_title = ""
+    __x = None
+    __y = None
 
     def __init__(self, window_name: str = "",
                  window_appearance: int = WINDOW_NATIVE,
@@ -36,21 +39,41 @@ class Window:
                  window_mode: int = WINDOW_NORMAL,
                  window_size: str = "500x500",
                  window_position: int = WINDOW_DEFAULT_POS):
-        self.x = 0
-        self.y = 0
-        self.window = tkinter.Tk()
-        self.window_title = "New Window"
+        self.__x = 0
+        self.__y = 0
+        self.__window = tkinter.Tk()
         self.set_name(window_name)
         self.set_mode(mode=window_mode, size=window_size)
         self.set_look(appearance=window_appearance, buttons=window_buttons, name=window_name)
         self.set_position(position=window_position, size=window_size)
 
+    def display(self):
+        self.__window.mainloop()
+
+    def dispose(self):
+        self.__window.destroy()
+
+    def __grab(self, event):
+        self.__x = event.x
+        self.__y = event.y
+
+    def __release(self, event):
+        self.__x = None
+        self.__y = None
+
+    def __move(self, event):
+        delta_x = event.x - self.__x
+        delta_y = event.y - self.__y
+        x = self.__window.winfo_x() + delta_x
+        y = self.__window.winfo_y() + delta_y
+        self.__window.geometry(f"+{x}+{y}")
+
     def set_name(self, window_name: str = ""):
         if not window_name:
             window_name = "New Window"
 
-        self.window.title(window_name)
-        self.window_title = window_name
+        self.__window.title(window_name)
+        self.__window_title = window_name
 
     def set_mode(self, mode: int = WINDOW_NORMAL, size: str = "500x500"):
         global WINDOW_FULL_SCREEN, WINDOW_HIDDEN, WINDOW_NORMAL
@@ -60,57 +83,57 @@ class Window:
                 raise Exception("Using WINDOW_NORMAL, size parameter must be defined too")
 
             # Set window size
-            self.window.geometry(size)
+            self.__window.geometry(size)
 
         elif mode == WINDOW_FULL_SCREEN:
-            self.window.wm_attributes('-fullscreen', 'true')
+            self.__window.wm_attributes('-fullscreen', 'true')
 
         elif mode == WINDOW_HIDDEN:
-            self.window.wm_attributes('-fullscreen', 'true')
-            self.window.wm_state("iconic")
+            self.__window.wm_attributes('-fullscreen', 'true')
+            self.__window.wm_state("iconic")
 
     def set_look(self, appearance: int = WINDOW_NATIVE, buttons: bool = False, name: str = "New Window"):
         global WINDOW_CUSTOM
 
         if appearance == WINDOW_CUSTOM:
-            self.window.wm_overrideredirect(True)
+            self.__window.wm_overrideredirect(True)
             self.display_actions_bar(name, buttons)
 
     def set_position(self, position: int = WINDOW_DEFAULT_POS, size: str = "500x500"):
         global WINDOW_CENTERED
 
         if position == WINDOW_CENTERED:
-            screen_width = self.window.winfo_screenwidth()
-            screen_height = self.window.winfo_screenheight()
+            screen_width = self.__window.winfo_screenwidth()
+            screen_height = self.__window.winfo_screenheight()
 
             window_width, window_height = size.split("x")
 
             screen_width = floor((int(screen_width) - int(window_width)) / 2)
             screen_height = floor((int(screen_height) - int(window_height)) / 2)
 
-            self.window.geometry(f"+{screen_width}+{screen_height}")
+            self.__window.geometry(f"+{screen_width}+{screen_height}")
 
     def display_actions_bar(self, navbar_title: str = "", navbar_buttons: bool = False):
         global ARIAL_SMALL, CURSOR_SQUARED
 
         # Window name
         handle = tkinter.Label(
-            self.window,
+            self.__window,
             text=navbar_title,
             font=ARIAL_SMALL
         )
         handle.pack(side="top", fill="both", pady=5)
 
         # Drag window functionality
-        handle.bind("<ButtonPress-1>", self.grab)
-        handle.bind("<ButtonRelease-1>", self.release)
-        handle.bind("<B1-Motion>", self.move)
+        handle.bind("<ButtonPress-1>", self.__grab)
+        handle.bind("<ButtonRelease-1>", self.__release)
+        handle.bind("<B1-Motion>", self.__move)
 
         # Add here your navigation bar components        
         if navbar_buttons:
             # Close window button
             tkinter.Button(
-                self.window,
+                self.__window,
                 width="2",
                 text="x",
                 bg="#d00",
@@ -119,25 +142,7 @@ class Window:
                 relief="flat",
                 cursor=CURSOR_SQUARED,
                 command=partial(self.dispose)
-            ).pack(anchor="w", side="top")
+            ).pack(anchor="w", side="top")    
 
-    def display(self):
-        self.window.mainloop()
-
-    def dispose(self):
-        self.window.destroy()
-
-    def grab(self, event):
-        self.x = event.x
-        self.y = event.y
-
-    def release(self, event):
-        self.x = None
-        self.y = None
-
-    def move(self, event):
-        delta_x = event.x - self.x
-        delta_y = event.y - self.y
-        x = self.window.winfo_x() + delta_x
-        y = self.window.winfo_y() + delta_y
-        self.window.geometry(f"+{x}+{y}")
+    def get_root(self):
+        return self.__window
