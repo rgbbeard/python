ab7cdbf377b7543f17c6a0fc752fb1746a592a02
rgb_beard <62804472+rgbbeard@users.noreply.github.com>
2022-12-21 08:30:54 +0100

    Update pwgen.py

--- pwgen.py
+++ pwgen.py
@@ -1,5 +1,19 @@
 import random, string
+from sys import argv
 
-chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + "%&$£/!=?^.;,:_-<>*+@#"
+include_special_chars: bool = False
+strlen = 15
 
-print(''.join(random.choice(chars) for i in range(15)))
+if len(argv) >= 2:
+	for a in argv:
+		if a == "--special-chars":
+			include_special_chars = True
+		elif "-len=" in a:
+			strlen = int(a.split("=")[1])
+
+chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
+
+if include_special_chars:
+	chars += "%&$£/!=?^.;,:_-<>*+@#"
+
+print(''.join(random.choice(chars) for i in range(strlen)))
